2454170599fe66f1949f5c0a2905124c5231220c
Mathieu Débit <mathieu.debit@gmail.com>
2016-05-08 14:59:33 +0200

    Add Admin & Approved users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   get 'home/index'
 
-  devise_for :users
-  root to: "home#index"
+  Rails.application.routes.draw do
+    devise_for :users, controllers: { sessions: 'users/sessions' }
+    # get :users, to: 'users/sessions#index'
+  end
 
   resources :participants
 end
